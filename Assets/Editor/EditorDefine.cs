59b115f0ce444256a0a49bfeb5d93cd0ebc3a26a
LAPTOP-FU1EMJB7\seven <seven.eleven.twenty@gmail.com>
2022-12-17 16:13:54 +0900

    磁力台にポイントを配置可能になった

--- Assets/Editor/EditorDefine.cs
+++ Assets/Editor/EditorDefine.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 namespace Path
 {
+    // プレハブ取得用のパス
     public static class PrefabPath
     {
         public static readonly string waiPoint = "Assets/Prefabs/Map/WaiPoint.prefab";
         public static readonly string stageRoute = "Assets/Prefabs/Map/StageRoute.prefab";
+        public static readonly string jumpRamp = "Assets/Prefabs/StageGimmick/JumpRamp.prefab";
 
     }
 
--- Assets/Editor/SetWaiPoint.cs
+++ Assets/Editor/SetWaiPoint.cs
@@ -10,6 +10,12 @@ public class SetWaiPoint : EditorWindow
 
     private GameObject parent;
     private GameObject prefab;
+    bool isSetJumpRamp;
+
+    // 振り分ける用
+    private GameObject jumpRampPrefab;
+    private List<GameObject> jumpRamps;
+
 
     // ウィンドウ
     [MenuItem("Window/Editor extention/SetWaiPoint", false, 1)]
@@ -17,7 +23,17 @@ public class SetWaiPoint : EditorWindow
     {
         SetWaiPoint window = GetWindow<SetWaiPoint>();
         window.titleContent = new GUIContent("SetWaiPoint Window");
+        window.Init();
 
+    }
+
+    // 初期値セット
+    private void Init()
+    {
+        parent = AssetDatabase.LoadAssetAtPath<GameObject>(PrefabPath.stageRoute);
+        prefab = AssetDatabase.LoadAssetAtPath<GameObject>(PrefabPath.waiPoint);
+        jumpRampPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(PrefabPath.jumpRamp);
+        isSetJumpRamp = true;
     }
 
     private void OnGUI()
@@ -29,17 +45,25 @@ public class SetWaiPoint : EditorWindow
         prefab = EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), true) as GameObject;
 
 
+        isSetJumpRamp = EditorGUILayout.Toggle("JumpRanp",isSetJumpRamp);
+
+
         // ボタンを押されたら
         if (GUILayout.Button("WaiPoint設置！"))
         {
             // 親オブジェクトのルートの生成
             GameObject route = PrefabUtility.InstantiatePrefab(parent) as GameObject;
+            // Ctr+Zで戻せるようにundoに追加
+            Undo.RegisterCreatedObjectUndo(route, "Create Route");
 
             // 子オブジェクトのウェイポイントの生成
-            var obj = PrefabUtility.InstantiatePrefab(prefab, route.transform);
+
+            InSceneCategorizeObjects();
+
+            SetJumpRampPoint(route);
             
-            Undo.RegisterCreatedObjectUndo(route, "Create Route");
-            Undo.RegisterCreatedObjectUndo(obj, "Create WaiPoint");
+            
+
 
         }
 
@@ -61,6 +85,40 @@ public class SetWaiPoint : EditorWindow
 
     }
 
+    private void InSceneCategorizeObjects()
+    {
+        var objects = Resources.FindObjectsOfTypeAll<GameObject>();
+        foreach (var obj in objects)
+        {
+            string path = AssetDatabase.GetAssetOrScenePath(obj);
+            bool isScene = path.Contains(".unity");
+            if (isScene)
+            {
+
+                if (isSetJumpRamp && PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(obj) == PrefabPath.jumpRamp)
+                {
+
+                    jumpRamps.Add(obj);
+
+
+                }
+                
+
+            }
+        }
+    }
+
+
+    private void SetJumpRampPoint(GameObject parent)
+    {
+        foreach (var item in jumpRamps)
+        {
+            var point = PrefabUtility.InstantiatePrefab(prefab, parent.transform) as GameObject;
+            point.transform.position = item.transform.position;
+            Undo.RegisterCreatedObjectUndo(point, "Create WaiPoint");
+
+        }
+    }
 
 
 }
