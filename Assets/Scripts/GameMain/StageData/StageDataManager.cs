0760657600ea19c554af21b6a5c92ddb7b3337a5
LAPTOP-FU1EMJB7\seven <seven.eleven.twenty@gmail.com>
2022-12-26 17:08:38 +0900

    データからオブジェクトの生成

--- Assets/Scripts/GameMain/StageData/StageDataManager.cs
+++ Assets/Scripts/GameMain/StageData/StageDataManager.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// 自作クラスの情報を使いたいためeditorフォルダには入れない
+#if UNITY_EDITOR
+using UnityEditor;
+
+using Path;
 
 public enum StagePartsCategory
 {
@@ -18,7 +23,7 @@ public class StagePart
 {
     public StagePartsCategory category;
     public Vector2 position;
-    public Vector2 scale;
+    public Vector2 sizeMagnification = Vector2.one;
     public bool isNorth;
 }
 
@@ -32,25 +37,51 @@ public class StageData
 }
 
 
-
-
-public class StageDataManager : MonoBehaviour
+public class StageDataManager : EditorWindow
 {
     [SerializeField]
     private StageData stageData;
+    //private GameObject parent;
+
+    //スクロール位置
+    private Vector2 _scrollPosition = Vector2.zero;
 
 
-
-    // Start is called before the first frame update
-    void Start()
+    // ウィンドウ
+    [MenuItem("Window/Editor extention/StageDataManager", false, 1)]
+    private static void ShowSetWaiPointWindow()
     {
-        GenerateStage();
+        StageDataManager window = GetWindow<StageDataManager>();
+        window.titleContent = new GUIContent("StageDataManager Window");
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnGUI()
     {
-        
+
+        //        parent = EditorGUILayout.ObjectField("Parent", parent, typeof(GameObject), true) as GameObject;
+        //描画範囲が足りなければスクロール出来るように
+        _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+
+        SerializedObject so = new SerializedObject(this);
+
+        so.Update();
+
+        SerializedProperty stringsProperty = so.FindProperty("stageData");
+
+        EditorGUILayout.PropertyField(stringsProperty, true);
+
+        so.ApplyModifiedProperties();
+
+
+        //スクロール箇所終了
+        EditorGUILayout.EndScrollView();
+
+        if (GUILayout.Button("ステージ生成！", GUILayout.Height(64)))
+        {
+            GenerateStage();
+        }
     }
 
     private void GenerateStage()
@@ -59,29 +90,79 @@ public class StageDataManager : MonoBehaviour
 
         foreach (var part in stageData.stageParts)
         {
-            switch (part.category)
+
+
+            InstantiateStagePart(part);
+        }
+
+    }
+
+    private void InstantiateStagePart(StagePart partData)
+    {
+        GameObject baseObject = GetPrefab(partData.category);
+        if (!baseObject) return;
+
+        GameObject gameObject = PrefabUtility.InstantiatePrefab(baseObject) as GameObject;
+        gameObject.transform.position = partData.position;
+        gameObject.transform.localScale *= partData.sizeMagnification;
+
+        if (IsSetTag(partData.category))
+        {
+            if (partData.isNorth)
             {
-                case StagePartsCategory.Scaffold:
-                    break;
-                case StagePartsCategory.JumpRamp:
-                    break;
-                case StagePartsCategory.Wall:
-                    break;
-                case StagePartsCategory.NormalWall:
-                    break;
-                case StagePartsCategory.ItemBox:
-                    break;
-                default:
-                    break;
+                gameObject.tag = Tag.PoleTag.north;
+            }
+            else
+            {
+                gameObject.tag = Tag.PoleTag.south;
+
             }
         }
 
+
+        Undo.RegisterCreatedObjectUndo(gameObject, "Create stageObject");
+
+
+    }
+
+    private GameObject GetPrefab(StagePartsCategory category)
+    {
+        string path = null;
+        switch (category)
+        {
+            case StagePartsCategory.Scaffold:
+                path = PrefabPath.scaffold;
+                break;
+            case StagePartsCategory.JumpRamp:
+                path = PrefabPath.jumpRamp;
+
+                break;
+            case StagePartsCategory.Wall:
+                path = PrefabPath.wall;
+
+                break;
+            case StagePartsCategory.NormalWall:
+                path = PrefabPath.normalWall;
+
+                break;
+            case StagePartsCategory.ItemBox:
+                path = PrefabPath.itemBox;
+
+                break;
+
+        }
+        return AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        
+    }
+
+    private bool IsSetTag(StagePartsCategory categry)
+    {
+        return categry == StagePartsCategory.JumpRamp || categry == StagePartsCategory.Wall;
     }
 
 
 
 
 
-
-
 }
+#endif
--- Assets/Scripts/Tools/EditorDefine.cs
+++ Assets/Scripts/Tools/EditorDefine.cs
@@ -11,6 +11,7 @@ namespace Path
         public static readonly string stageRoute = "Assets/Prefabs/Map/StageRoute.prefab";
         public const string jumpRamp = "Assets/Prefabs/StageGimmick/JumpRamp.prefab";
         public const string wall = "Assets/Prefabs/StageGimmick/Wall.prefab";
+        public const string normalWall = "Assets/Prefabs/StageGimmick/NormalWall.prefab";
         public const string scaffold = "Assets/Prefabs/StageGimmick/Scaffold.prefab";
         public const string itemBox = "Assets/Prefabs/StageGimmick/itemBox.prefab";
 
@@ -20,6 +21,17 @@ namespace Path
 
 }
 
+
+namespace Tag
+{
+    // プレハブ取得用のパス
+    public static class PoleTag
+    {
+        public static readonly string north = "North";
+        public static readonly string south = "South";
+
+    }
+}
 namespace Layer
 {
     public static class LayerNumber
