8aceebe59245c4b2cc39a66374018db7ffeca457
LAPTOP-FU1EMJB7\seven <seven.eleven.twenty@gmail.com>
2023-01-13 00:32:33 +0900

    add stopChaser

---
+++ Assets/Scripts/GameMain/Item/ChaserStopperController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserStopperController : MonoBehaviour
+{
+
+    [SerializeField, Min(0.0f)]
+    private float stopTime;
+
+    [SerializeField]
+    private GameObject backGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
